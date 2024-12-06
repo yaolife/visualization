08ad2864e19eb901a6340505770e8672b8971f6c
yaolife <yaolife@126.com>
2024-12-06 09:42:34 +0800

    样式调整

--- src/components/CustomModal/index.jsx
+++ src/components/CustomModal/index.jsx
@@ -1,40 +1,39 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Modal, Space, Toast } from 'antd-mobile';
-import styles from './index.less';
+import './index.less';
 const CustomModal = (props) => {
-  const { visible,handleClick,handleConfirm } = props;
-  const onClose=()=>{
+  const { visible, handleClick, handleConfirm } = props;
+  const onClose = () => {
     handleClick(false);
-  }
-  const onConfirm=()=>{
+  };
+  const onConfirm = () => {
     handleConfirm();
-  }
-useEffect(() => {
-  if (visible) {
-    Modal.alert({
-      title: '3333',
-      content: '右上角有个关闭的小图标，点击它也可以关闭弹窗',
-      showCloseButton: true,
-      visible:visible,
-      bodyClassName:'cardContent',
-      bodyStyle:{
-        border:'1px solid #FF0000',
-        height:280
-      },
-      confirmText:'查看并领取',
-      onConfirm:()=>{
-        onConfirm()
-      },
-      onClose:()=>{
-        onClose()
-      }
-    });
-  }
-}, [visible]);
-  return (
-    <>  
-         
-    </>
-  );
+  };
+  useEffect(() => {
+    if (visible) {
+      Modal.alert({
+        content: (
+          <div className='cardTips'>
+            <span>查询到作业票</span>
+            <span>【1454664】</span>
+          </div>
+        ),
+        showCloseButton: true,
+        visible: visible,
+        bodyClassName: 'cardContent',
+        bodyStyle: {
+          height: 280,
+        },
+        confirmText: '查看并领取',
+        onConfirm: () => {
+          onConfirm();
+        },
+        onClose: () => {
+          onClose();
+        },
+      });
+    }
+  }, [visible]);
+  return <></>;
 };
 export default CustomModal;
