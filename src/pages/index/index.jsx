8677ff2b5b24f3a9a46d5bbf964be6409aca5b1f
yaolife <yaolife@126.com>
2024-12-17 14:17:42 +0800

    首页图片做懒加载

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,6 @@
 import area from '@/images/area.png';
 import Layout from '@/layout';
+import LazyLoad from 'react-lazyload';
 import { connectMQTT, disconnectMQTT, subscribeMQTT } from '@/services/services';
 import { Image } from 'antd-mobile';
 import { useEffect, useRef, useState } from 'react';
@@ -38,25 +39,23 @@ const Index = () => {
       .then(() => {
         // 订阅主题 人员的
         subscribeMQTT('realTimeWorker', (message) => {
-          console.log('订阅的信息:', message);
           try {
             const parsedMessage = JSON.parse(message);
-            console.log('解析后的消息:', parsedMessage);
-
             // 检查消息格式
             if (Array.isArray(parsedMessage) && parsedMessage.length > 0) {
               const firstMessage = parsedMessage[0];
               if (firstMessage.personId && firstMessage.latitude && firstMessage.longitude) {
                 setMessages((prevMessages) => {
                   // 检查是否存在相同的 personId 并更新
-                  const existingIndex = prevMessages.findIndex(msg => msg.personId === firstMessage.personId);
+                  const existingIndex = prevMessages.findIndex(
+                    (msg) => msg.personId === firstMessage.personId,
+                  );
                   if (existingIndex !== -1) {
                     const updatedMessages = [...prevMessages];
                     updatedMessages[existingIndex] = firstMessage;
                     console.log(updatedMessages, 'updatedMessages');
                     return updatedMessages;
                   } else {
-                    console.log(prevMessages, firstMessage, 'prevMessages___parsedMessage');
                     return [...prevMessages, firstMessage];
                   }
                 });
@@ -84,14 +83,14 @@ const Index = () => {
               if (firstMessage.vehicleNumber && firstMessage.latitude && firstMessage.longitude) {
                 setVehicleMessages((prevMessages) => {
                   // 检查是否存在相同的 vehicleNumber(车牌号) 并更新
-                  const existingIndex = prevMessages.findIndex(msg => msg.vehicleNumber === firstMessage.vehicleNumber);
+                  const existingIndex = prevMessages.findIndex(
+                    (msg) => msg.vehicleNumber === firstMessage.vehicleNumber,
+                  );
                   if (existingIndex !== -1) {
                     const updatedMessages = [...prevMessages];
                     updatedMessages[existingIndex] = firstMessage;
-                    console.log(updatedMessages, 'updatedMessages');
                     return updatedMessages;
                   } else {
-                    console.log(prevMessages, firstMessage, 'prevMessages___parsedMessage');
                     return [...prevMessages, firstMessage];
                   }
                 });
@@ -107,14 +106,13 @@ const Index = () => {
         });
       })
       .catch((error) => {
-        console.log(error, 'error');
         console.error('Failed to connect to MQTT broker:', error);
       });
 
-        // 清理函数，在组件卸载时断开连接
-        return () => {
-          disconnectMQTT();
-        };
+    // 清理函数，在组件卸载时断开连接
+    return () => {
+      disconnectMQTT();
+    };
   }, []);
 
   useEffect(() => {
@@ -128,7 +126,9 @@ const Index = () => {
   }, [messages, imagePosition]);
 
   useEffect(() => {
-    const newVehiclePointPositions = vehicleMessages.map((msg) => calculatePointPosition(msg, imagePosition));
+    const newVehiclePointPositions = vehicleMessages.map((msg) =>
+      calculatePointPosition(msg, imagePosition),
+    );
     setVehiclePointPositions(newVehiclePointPositions);
   }, [vehicleMessages, imagePosition]);
 
@@ -157,9 +157,9 @@ const Index = () => {
     const pixelX = relativeLongitude * imageWidth;
     const pixelY = (1 - relativeLatitude) * imageHeight; // 注意：Y轴是从上到下的
 
-    console.log(`计算结果: relativeLatitude=${relativeLatitude}, relativeLongitude=${relativeLongitude}`);
-    console.log(`计算结果: pixelX=${pixelX}, pixelY=${pixelY}`);
-
+    console.log(
+      `计算结果: relativeLatitude=${relativeLatitude}, relativeLongitude=${relativeLongitude}`,
+    );
     return { x: pixelX, y: pixelY };
   };
 
@@ -200,7 +200,9 @@ const Index = () => {
           top: '0px',
         }}
       >
-        <Image src={area} width={imageWidth} height={imageHeight} />
+        <LazyLoad>
+          <Image src={area} width={imageWidth} height={imageHeight} />
+        </LazyLoad>
       </div>
       {pointPositions.map((position, index) => (
         <div
