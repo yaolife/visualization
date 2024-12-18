21f71060928bdc2d81b757fb3b7f8f7ec75b7fcf
yaolife <yaolife@126.com>
2024-12-18 16:51:41 +0800

    去掉打印

--- src/pages/myTickets/index.jsx
+++ src/pages/myTickets/index.jsx
@@ -10,20 +10,6 @@ import styles from './index.less';
 const MyTicket = () => {
   const [value, setValue] = useState('');
   const [visible, setVisible] = useState(false);
-  const [ticketList, setTicketList] = useState([
-    { content: 'FJ4315665', title: '作业票号', id: '67' },
-    { content: '设备阀门检修长字段显示长字段显示长字段显示', title: '作业内容', id: '36' },
-    { content: 'SJ2132H,冷却阀门', title: '作业设备', id: '64' },
-    { content: '5mx  ， L3   ， R09', title: '作业位置', id: '56' },
-    {
-      content: '【胡广华】,【张是三】,【胡广华】,【曾茉莉】,【张是三】',
-      title: '作业人员',
-      id: '29',
-    },
-    { content: 'SJ2132H418501', title: '定位卡号', id: '99' },
-    { content: '2024-06-09  23:59', title: '开始时间', id: '78' },
-    { content: '2024-06-10  23:59', title: '结束时间', id: '32' },
-  ]);
 
   const goNavigation = () => {
     history.push('/ticketNavigation');
@@ -35,7 +21,6 @@ const MyTicket = () => {
   useEffect(() => {
     // 设置底部导航栏的激活状态
     if (activeTab !== undefined) {
-      console.log(activeTab, 'activeTab');
       history.replace({ pathname: '/myTickets', state: { activeTab } });
     }
   }, [activeTab]); // 移除了 history 依赖
