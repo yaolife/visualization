50ac58459912a17b8d16346d08c26bdc249b1a16
yaolife <yaolife@126.com>
2024-12-18 11:57:01 +0800

    作业票领取

--- src/pages/collectionTickets/index.jsx
+++ src/pages/collectionTickets/index.jsx
@@ -3,21 +3,12 @@ import React, { useState } from 'react';
 import { Image, Input, Button } from 'antd-mobile';
 import Header from '@/components/Navbar';
 import { history } from 'umi';
-import TicketItem from '@/components/TicketItem';
 import UsModal from '@/components/UsModal';
 import styles from './index.less';
 
 const Ticket = () => {
   const [value, setValue] = useState('');
   const [visible, setVisible] = useState(false);
-  const [ticketList, setTicketList] = useState([
-    { content: 'FJ4315665', title: '作业票号', id: '67' },
-    { content: '设备阀门检修长字段显示长字段显示长字段显示', title: '作业内容', id: '6' },
-    { content: 'SJ2132H,冷却阀门', title: '作业设备', id: '64' },
-    { content: '5mx  ， L3   ， R09', title: '作业位置', id: '56' },
-    { content: '2024-06-09  23:59', title: '开始时间', id: '78' },
-    { content: '2024-06-10  23:59', title: '结束时间', id: '32' },
-  ]);
 
   const receiveCard = () => {
     setVisible(true);
